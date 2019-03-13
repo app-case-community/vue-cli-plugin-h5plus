e3fbdb0c1f4b8e63d7cf26ba0da94d3305fcd46e
zhuzhe <zhuzhe@findpiano.cn>
2019-03-13 13:35:44 +0800

    fix

--- generator/index.js
+++ generator/index.js
@@ -1,2 +1,13 @@
 module.exports = (api, opts, rootOptions) => {
+    api.extendPackage({
+        scripts: {
+            "build:android": "cross-env PLATFORM_ENV=android vue-cli-service build",
+            "build:ios": "cross-env PLATFORM_ENV=ios vue-cli-service build"
+        },
+        devDependencies: {
+            "cross-env": "^5.2.0",
+            "fs-extra": "^7.0.1",
+        }
+    })
+    api.render('./templates')
 }
